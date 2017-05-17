57c11754172d66cd201001333be59d306705cb44
constantine-vainstein <costa@campus.technion.ac.il>
2017-05-17 22:07:39 +0300

    Oh! Yes the scripts...

---
+++ lib/hw/contrib/cores/spad_controller/tcl/spad_manager_prj.tcl
@@ -0,0 +1,35 @@
+
+# get the directory where this script resides
+set thisDir [file dirname [info script]]
+# source common utilities
+source -notrace $thisDir/utils.tcl
+
+# Create project
+
+
+set hdlRoot ../hdl/sources_1/new
+set simRoot ../hdl/sim_1/new
+set xdcRoot ../xdc
+
+# Create project
+create_project -force spad_manager . -part xc7k70tfbv676-1
+# xc7vx690tffg1761-3 
+# 
+
+# Set project properties
+set obj [get_projects top]
+set_property "simulator_language" "Mixed" $obj
+set_property "target_language" "Verilog" $obj
+
+add_files -norecurse $hdlRoot/controller.v
+add_files -norecurse $hdlRoot/manager.v
+add_files -norecurse $hdlRoot/read_process_manager.v
+add_files -norecurse $hdlRoot/spad_controller_definitions.vh
+
+# Add Simulation Sources
+add_files -fileset sim_1 -norecurse $simRoot/spad_ctrl_tb.v
+add_files -fileset sim_1 -norecurse $simRoot/spad_manager_tb.v
+add_files -fileset sim_1 -norecurse $simRoot/spad_sim.v
+
+# If successful, "touch" a file so the make utility will know it's done 
+touch {.project.done}
