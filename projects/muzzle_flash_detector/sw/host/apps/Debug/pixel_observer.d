543cecacc531f22a2d3ad66135414a04c432c777
Constantine Vainstein <costa@campus.technion.ac.il>
2017-11-26 01:44:52 +0200

    Implement the pixel_observer application, run in command line.

---
+++ projects/muzzle_flash_detector/sw/host/apps/Debug/pixel_observer.d
@@ -0,0 +1,1 @@
+pixel_observer.d: ../pixel_observer.c
--- projects/muzzle_flash_detector/sw/host/apps/pixel_observer.c
+++ projects/muzzle_flash_detector/sw/host/apps/pixel_observer.c
@@ -20,7 +20,19 @@
 #define MIN_COL 0
 #define MAX_COL 63
 
+#define COL_SIZE 64
+#define ROW_SIZE 64
+
 #define ETHERNET_FRMS_PER_CAMERA_FRM 65
+#define PIXEL_BLOCK_PACKET_LEN 81
+
+#define ETHER_ADDR_LEN_BYTES 6
+
+#define PIXEL_BLOCK_HEIGHT 8
+#define PIXEL_BLOCK_WIDTH 8
+#define LAST_BLOCK_ID (PIXEL_BLOCK_HEIGHT * PIXEL_BLOCK_WIDTH - 1)
+
+#define FRAME_START_PREAMABLE 0xcccccccc
 
 typedef struct InputArgs
 {
@@ -30,6 +42,63 @@ typedef struct InputArgs
 	unsigned int framesToCapture;
 } InputArgs;
 
+typedef struct Frame
+{
+	unsigned int frameId;
+	unsigned char pixels[ROW_SIZE][COL_SIZE];
+} Frame;
+
+typedef enum ObserverState
+{
+	WAITING_FOR_FIRST_FRAME,
+	MONITORING_FRAMES
+} ObserverState;
+
+typedef struct PixelObserver
+{
+	InputArgs args;
+	Frame * monitoredFrames;
+	unsigned int frameCount;
+	unsigned int blockCount;
+	ObserverState state;
+} PixelObserver;
+
+typedef struct EthHeader
+{
+	u_char da[ETHER_ADDR_LEN_BYTES];
+	u_char sa[ETHER_ADDR_LEN_BYTES];
+	u_short etherType;
+} EthHeader /*__attribute__((packed))*/;
+
+typedef struct FrameIdMessage
+{
+	EthHeader ethHeader;
+	u_short spare;
+	u_int preamable;
+	u_int frameId;
+} FrameIdMessage /*__attribute__((packed))*/;
+
+typedef struct PixelBlockMessage
+{
+	EthHeader ethHeader;
+	u_short spare;
+	u_char blockId;
+	u_char pixels[PIXEL_BLOCK_WIDTH][PIXEL_BLOCK_HEIGHT];
+} PixelBlockMessage /*__attribute__((packed))*/;
+
+
+static const PixelObserver initObsever = {
+		{
+				NULL,
+				DEFAULT_ROW,
+				DEFAULT_COL,
+				FRAMES_TO_CAPTURE_BY_DEFAULT
+		},
+		NULL,
+		0,
+		0,
+		WAITING_FOR_FIRST_FRAME
+};
 
 
 
@@ -38,26 +107,58 @@ typedef struct InputArgs
 /* hexadecimal.                                                                */
 void processPacket(u_char *arg, const struct pcap_pkthdr* pkthdr, const u_char * packet){
 
- int i=0, *counter = (int *)arg;
+	PixelObserver *observer = (PixelObserver *)arg;
+	const FrameIdMessage * frmIdMsg = (FrameIdMessage *)packet;
+	const PixelBlockMessage * pxlBlkMsg = (PixelBlockMessage *)packet;
+	u_int col;
+	u_int row;
+	u_int col_offset;
+	u_int row_offset;
 
- printf("Packet Count: %d\n", ++(*counter));
- printf("Received Packet Size:inputArgs %d\n", pkthdr->len);
- printf("Payload:\n");
- for (i=0; i<pkthdr->len; i++){
+	if(observer->frameCount < observer->args.framesToCapture)
+	{
+		switch (observer->state)
+		{
+		case WAITING_FOR_FIRST_FRAME:
+			if(frmIdMsg->preamable == FRAME_START_PREAMABLE)
+			{
+				observer->state = MONITORING_FRAMES;
+				observer->monitoredFrames[observer->frameCount].frameId = frmIdMsg->frameId;
+				observer->blockCount = 0;
+			}
+			else
+			{
+				/* do nothing, remain in the same state */
+			}
+			break;
 
-    if ( isprint(packet[i]) ) /* If it is a printable character, print it */
-        printf("%c ", packet[i]);
-    else
-        printf(". ");
+		case MONITORING_FRAMES:
 
-     if( (i%16 == 0 && i!=0) || i==pkthdr->len-1 )
-        printf("\n");
-  }
+			row_offset = (pxlBlkMsg->blockId / 8) * 8;
+			col_offset = (pxlBlkMsg->blockId % 8) * 8;
+			for(row = 0; row < PIXEL_BLOCK_HEIGHT; row++)
+			{
+				for(col = 0; col < PIXEL_BLOCK_WIDTH; col++)
+				{
+					observer->monitoredFrames[observer->frameCount].pixels[row_offset + row][col_offset + col] =
+							pxlBlkMsg->pixels[col][row];
+				}
+			}
+			if(pxlBlkMsg->blockId == LAST_BLOCK_ID)
+			{
+				observer->state = WAITING_FOR_FIRST_FRAME;
+				observer->frameCount++;
+			}
+
+			break;
+		}
+	}
+
  return;
 }
 
 /* Fill input arguments by data from command line or default data */
-void fillArgs(int argc, const char *argv[], char * errbuf, InputArgs * inputArgs)
+void fillArgs(int argc, char *argv[], char * errbuf, InputArgs * inputArgs)
 {
 
 	/* Fill device */
@@ -128,27 +229,47 @@ void fillArgs(int argc, const char *argv[], char * errbuf, InputArgs * inputArgs
 
 }
 
+void printDesiredAddresOverTime(PixelObserver * me)
+{
+	u_int frameCount = 0;
+	u_int pixelNum = 0;
+
+	printf("\t  Frm Id\tPixel 0\tPixel 1\tPixel 2\tPixel 3\tPixel 4\tPixel 5\tPixel 6\tPixel 7\n");
+	printf("---------------------------------------------------------------------------------------\n");
+	for(frameCount = 0; frameCount < me->args.framesToCapture ; frameCount++)
+	{
+		printf("Frm #%2d\t",frameCount);
+		printf("%8X\t",me->monitoredFrames[frameCount].frameId);
+		for(pixelNum = 0; pixelNum < PIXEL_BLOCK_HEIGHT; pixelNum++)
+		{
+			printf("%4d\t",me->monitoredFrames[frameCount].pixels[me->args.rowToCapture * 8 + pixelNum][me->args.colToCapture]);
+		}
+		printf("\n");
+	}
+}
+
 
 
 /* main(): Main function. Opens network interface and calls pcap_loop() */
 int main(int argc, char *argv[] ){
 
- int i=0, count=0;
  pcap_t *descr = NULL;
- char errbuf[PCAP_ERRBUF_SIZE], *device=NULL;
- InputArgs inputArgs;
+ char errbuf[PCAP_ERRBUF_SIZE];
+ PixelObserver pxlObserver = initObsever;
  int pcapLoopReturn;
 
  memset(errbuf,0,PCAP_ERRBUF_SIZE);
 
 
- fillArgs(argc, argv, errbuf, &inputArgs);
+ fillArgs(argc, argv, errbuf, &(pxlObserver.args));
+ /* allocate space for frames */
+ pxlObserver.monitoredFrames = (Frame *)malloc(pxlObserver.args.framesToCapture * sizeof(Frame));
 
 
- printf("Opening device %s\n", inputArgs.device);
+ printf("Opening device %s\n", pxlObserver.args.device);
 
  /* Open device in promiscuous mode */
- if ( (descr = pcap_open_live(inputArgs.device, MAXBYTES2CAPTURE, 1,  512, errbuf)) == NULL){
+ if ( (descr = pcap_open_live(pxlObserver.args.device, MAXBYTES2CAPTURE, 1,  512, errbuf)) == NULL){
     fprintf(stderr, "ERROR: %s\n", errbuf);
     exit(1);
  }
@@ -156,14 +277,17 @@ int main(int argc, char *argv[] ){
  /* Loop & call processPacket() for every received packet*/
  pcapLoopReturn = pcap_loop(
 		 	 	 	 	 	 descr,
-		 	 	 	 	 	 (int)((inputArgs.framesToCapture + 1) * ETHERNET_FRMS_PER_CAMERA_FRM),
+		 	 	 	 	 	 (int)((pxlObserver.args.framesToCapture + 1) * ETHERNET_FRMS_PER_CAMERA_FRM),
 		 	 	 	 	 	 /* framesToCapture + 1 because may be we are in the middle of a frame */
 		 	 	 	 	 	 processPacket,
-		 	 	 	 	 	 (u_char *)&inputArgs);
+		 	 	 	 	 	 (u_char *)&pxlObserver);
  if (pcapLoopReturn == -1){
     fprintf(stderr, "ERROR: %s\n", pcap_geterr(descr) );
     exit(1);
  }
+
+ printDesiredAddresOverTime(&pxlObserver);
+free(pxlObserver.monitoredFrames);
 
 return 0;
 
